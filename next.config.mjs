2b7b78314df6e0befad853d718ab11cb6202376c
Rashid Ameer <r.ameer.offical@gmail.com>
2025-11-14 11:36:33 +0500

    Refactor next.config.js for improved readability and consistency

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,19 @@
-/** @type {import('next').NextConfig} */
+// next.config.js
 const nextConfig = {
   experimental: {
-    staleTimes: {
-      dynamic: 30,
-    },
+    staleTimes: { dynamic: 30 },
   },
   serverExternalPackages: ["@node-rs/argon2"],
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "*.ufs.sh",
-        pathname: `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/*`,
+        hostname: "**.ufs.sh", // Wildcard subdomain
+        pathname: `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/**`, // ** for deep paths
       },
     ],
   },
-  rewrites: () => {
-    return [{ source: "/hashtag/:tag", destination: "/search?q=%23:tag" }];
-  },
+  rewrites: () => [{ source: "/hashtag/:tag", destination: "/search?q=%23:tag" }],
 };
 
 export default nextConfig;
